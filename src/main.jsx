a9f1304668eea2631b4e4323b0de805f7e8547c4
Alok Kumar Jha <jhaalok1997@gmail.com>
2024-09-07 15:13:19 +0530

    some minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import {  createBrowserRouter,RouterProvider } from 'react-router-dom'
+import {  createBrowserRouter,RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import ContactUs from './Header/Contactus'
+import Home from './Header/Home.jsx'
+import Solution from './Header/Solution.jsx'
 
-/*const router = createBrowserRouter([
+const router = createBrowserRouter([
      {
       path:'/',
-      element:""
+      element:<App />,
      },
      {
-
-     }
+      path: '/Home',
+      element: <Home/>
+     },
+     {
+      path:"/ContactUs",
+      element:<ContactUs/>,
+     },
+     {
+      path:"/Solution",
+      element:<Solution/>,
+     },
 ])
   
- */ 
+ 
 
 
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
-    <App />
+     <RouterProvider router = {router}/>
+  
   </StrictMode>,
 )
-// <RouterProvider router = {router}/>
